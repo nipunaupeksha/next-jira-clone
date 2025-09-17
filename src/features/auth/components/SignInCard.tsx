2d6919d14148a424f6d605faf2a4cd2bb4a7dd77
nipunaupeksha <nipuna.upeksha@gmail.com>
2025-09-17 22:00:51 +0300

    create sign-in card

--- src/features/auth/components/SignInCard.tsx
+++ src/features/auth/components/SignInCard.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
 export const SignInCard = () => {
-    return <div>Sign In Card</div>;
+    return (
+        <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+            <CardHeader className="flex items-center justify-center text-center p-7">
+                <CardTitle className="text-2xl">
+                    Welcome Back!
+                </CardTitle>
+            </CardHeader>
+            <div className="px-7">
+                <DottedSeparator/>
+            </div>
+            <CardContent className="p-7">
+                <form className="space-y-4">
+                    <Input required type="email" value={""} onChange={()=>{}} placeholder="Enter your email address" disabled={false}/>
+                </form>
+            </CardContent>
+        </Card>
+    );
 }

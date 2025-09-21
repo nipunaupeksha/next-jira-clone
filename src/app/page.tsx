5f75dc6e2734f0eb8a4d396651683db3755230c3
nipunaupeksha <nipuna.upeksha@gmail.com>
2025-09-21 11:02:52 +0300

    adding session middleware and fixing auth logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
     <div>
-      <Button variant="primary">Click me</Button>
-      <Button variant="destructive">Click me</Button>
-      <Button variant="outline">Click me</Button>
-      <Button variant="secondary">Click me</Button>
-      <Button variant="tertiary">Click me</Button>
-      <Button variant="muted">Click me</Button>
-      <Button variant="ghost">Click me</Button>
+      Only visible to authorized users.
+      <Button onClick = {()=> mutate()}>Logout</Button>
     </div>
-  )
+  );
 }

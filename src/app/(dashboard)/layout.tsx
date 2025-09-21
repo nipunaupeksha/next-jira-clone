4f6d247c9d98e7e60bc86efb9249d2fb6f8bcfea
nipunaupeksha <nipuna.upeksha@gmail.com>
2025-09-21 17:09:48 +0300

    implement dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
 
 interface DashboardLayoutProps {
@@ -9,11 +10,11 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
     <div className="min-h-screen">
       <div className="flex w-full h-full">
         <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
-            <Sidebar/>
+          <Sidebar />
         </div>
-        <div className="lg:pl-[264px]">
+        <div className="lg:pl-[264px] w-full">
           <div className="mx-auto max-w-screen-2xl h-full">
-            {/* TODO: Navbar */}
+            <Navbar />
             <main className="h-full py-8 px-6 flex flex-col">{children}</main>
           </div>
         </div>

ec9ba037523128c382548e80b6478d128718a16e
nipunaupeksha <nipuna.upeksha@gmail.com>
2025-09-22 01:39:00 +0300

    create workspaces page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,15 +1,15 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/actions";
+import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
 
 export default async function Home() {
-
   const user = await getCurrent();
-  if(!user) redirect("/sign-in");
+  if (!user) redirect("/sign-in");
 
   return (
-    <div>
-      This is a home page
+    <div className="bg-neutral-500 p-4 h-full">
+      <CreateWorkspaceForm />
     </div>
   );
 }
---
+++ src/features/workspaces/server/route.ts
@@ -0,0 +1,31 @@
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+
+import { sessionMiddleware } from "@/lib/session-middleware";
+
+import { createWorkSpaceSchema } from "../schemas";
+import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { ID } from "node-appwrite";
+
+const app = new Hono().post(
+  "/",
+  zValidator("json", createWorkSpaceSchema),
+  sessionMiddleware,
+  async (c) => {
+    const databases = c.get("databases");
+    const user = c.get("user");
+
+    const { name } = c.req.valid("json");
+    const workspace = await databases.createDocument(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      ID.unique(),
+      {
+        name,
+      }
+    );
+    return c.json({ data: workspace });
+  }
+);
+
+export default app;
